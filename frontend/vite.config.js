3bf6fa101f691b081af4db3e55f953e456135f23
Pradeep1205k <pradeep1205k@gmail.com>
2026-01-20 22:24:07 +0530

    build fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/VRF_Converter/',
   plugins: [react()],
   server: {
     port: 5173
   }
-});
+})
